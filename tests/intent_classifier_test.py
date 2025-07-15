45920cba9718599a541e3f5c0ecc6d3c59e37c32
AlessandroCasonato <alessandro.casonato.dev@gmail.com>
2025-07-15 23:28:56 +0200

    Update intent_classifier_test.py

--- tests/intent_classifier_test.py
+++ tests/intent_classifier_test.py
@@ -5,6 +5,26 @@ def test_intent_classification_weather():
     intent = classifier.classify("What's the weather like in Rome?")
     assert intent == "weather"
 
+def test_intent_classification_temperature():
+    classifier = IntentClassifier()
+    intent = classifier.classify("What temperature there will in London tomorrow?")
+    assert intent == "temperature"
+
+def test_intent_classification_windy():
+    classifier = IntentClassifier()
+    intent = classifier.classify("How strong will be the wind on Friday in New York?")
+    assert intent == "wind"
+
+def test_intent_classification_rain():
+    classifier = IntentClassifier()
+    intent = classifier.classify("Is it raining in Florence?")
+    assert intent == "rain"
+
+def test_intent_classification_humidity():
+    classifier = IntentClassifier()
+    intent = classifier.classify("Can you tell me what's the current humidity in Paris?")
+    assert intent == "humidity"
+
 def test_intent_classification_help():
     classifier = IntentClassifier()
     intent = classifier.classify("What can you do?")
